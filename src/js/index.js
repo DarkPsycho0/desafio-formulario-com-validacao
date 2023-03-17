523c7eac9c0f2c97840b0356e48e5d872f189426
Matheus <13matheusilva@gmail.com>
2023-03-17 13:43:32 -0300

    js alterado

--- src/js/index.js
+++ src/js/index.js
@@ -4,19 +4,18 @@ const formulario = document.querySelector('form')
 
 formulario.addEventListener("submit", (form) => {
     form.preventDefault()
-    inputs.forEach(element => {
-        submitBtn.addEventListener('click', () => {
-            if (element.value === "") {
-                if (element.classList === "preenchido"){
-                    element.classList.remove('preenchido')
-                }
-                element.classList.add('erro')
-                element.nextElementSibling.classList.remove('none')
-            } else {
-                element.classList.add('preenchido')
-                element.classList.remove('erro')
-                element.nextElementSibling.classList.add('none')
-            }
-        })
-    });
 })
+
+inputs.forEach(element => {
+    submitBtn.addEventListener('click', () => {
+        if (element.value === "") {
+            element.classList.remove('preenchido')
+            element.classList.add('erro')
+            element.nextElementSibling.classList.remove('none')
+        } else {
+            element.classList.add('preenchido')
+            element.classList.remove('erro')
+            element.nextElementSibling.classList.add('none')
+        }
+    })
+});
