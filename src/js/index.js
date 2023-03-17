e004ca62beb6270bcbac2dab9c0cb05b4f19826d
Matheus <13matheusilva@gmail.com>
2023-03-17 12:26:40 -0300

    js pronto

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const submitBtn = document.querySelector('.submit')
+const inputs = document.querySelectorAll('.input')
+const formulario = document.querySelector('form')
+
+formulario.addEventListener("submit", (form) => {
+    form.preventDefault()
+    inputs.forEach(element => {
+        submitBtn.addEventListener('click', () => {
+            if (element.value === "") {
+                if (element.classList === "preenchido"){
+                    element.classList.remove('preenchido')
+                }
+                element.classList.add('erro')
+                element.nextElementSibling.classList.remove('none')
+            } else {
+                element.classList.add('preenchido')
+                element.classList.remove('erro')
+                element.nextElementSibling.classList.add('none')
+            }
+        })
+    });
+})
